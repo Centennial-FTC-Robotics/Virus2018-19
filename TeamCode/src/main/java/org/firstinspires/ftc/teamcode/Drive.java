33035e13a372d01ca776baba084f0d3ee1c0877b
baconsuperguy <keertikbacon@yahoo.com>
2018-11-14 16:17:26 -0500

    Slide limits set

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drive.java
@@ -12,13 +12,27 @@ public class Drive extends VirusMethods {
     public void runOpMode() throws InterruptedException {
         super.runOpMode();
         while(opModeIsActive()){
-            lmotor0.setPower(Range.clip(Math.abs(gamepad1.left_stick_y)*gamepad1.left_stick_y-Math.abs(gamepad1.right_stick_x)*gamepad1.right_stick_x,-1,1));
-            rmotor0.setPower(Range.clip(Math.abs(gamepad1.left_stick_y)*gamepad1.left_stick_y+Math.abs(gamepad1.right_stick_x)*gamepad1.right_stick_x, -1,1));
-            lmotor1.setPower(Range.clip(Math.abs(gamepad1.left_stick_y)*gamepad1.left_stick_y-Math.abs(gamepad1.right_stick_x)*gamepad1.right_stick_x,-1,1));
-            rmotor1.setPower(Range.clip(Math.abs(gamepad1.left_stick_y)*gamepad1.left_stick_y+Math.abs(gamepad1.right_stick_x)*gamepad1.right_stick_x, -1,1));
+            telemetry.update();
+//            lmotor0.setPower(Range.clip(Math.abs(gamepad1.left_stick_y)*gamepad1.left_stick_y-Math.abs(gamepad1.right_stick_x)*gamepad1.right_stick_x,-1,1));
+//            rmotor0.setPower(Range.clip(Math.abs(gamepad1.left_stick_y)*gamepad1.left_stick_y+Math.abs(gamepad1.right_stick_x)*gamepad1.right_stick_x, -1,1));
+//            lmotor1.setPower(Range.clip(Math.abs(gamepad1.left_stick_y)*gamepad1.left_stick_y-Math.abs(gamepad1.right_stick_x)*gamepad1.right_stick_x,-1,1));
+//            rmotor1.setPower(Range.clip(Math.abs(gamepad1.left_stick_y)*gamepad1.left_stick_y+Math.abs(gamepad1.right_stick_x)*gamepad1.right_stick_x, -1,1));
             slideLeft.setPower(gamepad2.left_stick_y);
             slideRight.setPower(gamepad2.left_stick_y);
+
+            if (0 != gamepad2.left_stick_y) {
+                if (slideLeft.getCurrentPosition()<-7300) {
+                    slides(-7290);
+                } else if(slideLeft.getCurrentPosition()>10 ){
+                    slides(0);
+                } else {
+                    slidePower(gamepad2.left_stick_y);
+                }
+            }
             hinge.setPower(gamepad2.right_stick_y);
+            //max -7300 min 0
+            telemetry.addData("Left slide",  slideLeft.getCurrentPosition());
+            telemetry.addData("Right slide",  slideRight.getCurrentPosition());
 
             idle();
         }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VirusMethods.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VirusMethods.java
@@ -1,8 +1,28 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
+
 /**
  * Created by keert on 10/26/2018.
  */
 
 public class VirusMethods extends VirusHardware {
+    public void slides(int position){
+        slideLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        slideRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        slideLeft.setPower(-1);
+        slideRight.setPower(-1);
+        slideLeft.setTargetPosition(position);
+        slideRight.setTargetPosition(position);
+        if (!slideLeft.isBusy() || !slideRight.isBusy()) {
+            slideLeft.setPower(0);
+            slideRight.setPower(0);
+        }
+    }
+    public void slidePower(double power){
+        slideLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        slideRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        slideLeft.setPower(power);
+        slideRight.setPower(power);
+    }
 }
