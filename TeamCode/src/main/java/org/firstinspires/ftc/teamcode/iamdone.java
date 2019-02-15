b8879ecd11ef7e1b9e830063572c59da63d5006e
baconsuperguy <keertikbacon@yahoo.com>
2019-02-15 16:56:18 -0500

    intake pivot up when hinge up

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/iamdone.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/iamdone.java
@@ -7,7 +7,7 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 @Autonomous(name="iamdone", group="Autonomous")
 
 /**
- * Created by keert on 1/25/2019.
+ * Created by keerts on 1/25/2019.
  */
 
 public class iamdone extends VirusMethods{
